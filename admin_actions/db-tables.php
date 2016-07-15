5d02b490c9c7f2a8aac5ae2709222acc19722d44
Attogram <barberi.office@gmail.com>
2016-07-15 18:56:07 +0200

    fix tablenames

--- admin_actions/db-tables.php
+++ admin_actions/db-tables.php
@@ -1,4 +1,4 @@
-<?php // Attogram Framework - Database Module - Database Tables v0.0.8
+<?php // Attogram Framework - Database Module - Database Tables v0.0.9
 
 namespace Attogram;
 
@@ -14,7 +14,8 @@ if (!$this->database->loadTableDefinitions() || !$this->database->tables) {
 
 foreach ($this->database->tables AS $tableName => $table_definition) {
     $count = $this->database->getTableCount($tableName);
-    print '<p><strong>$tableName</strong> table: <code>'.$count.'</code> entries'
+    $table
+    print '<p><h2 style="display:inline;">'.$tableName.'</h3><br /><code>'.$count.'</code> entries'
         . '<textarea class="form-control" rows="8">'.$table_definition.'</textarea></p>';
 }
 
